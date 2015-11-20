4312aadcad4ee7ee556e12e3bf20d6d80ab2a9c0
Matt Osborn <matthew.osborn@theguardian.com>
2015-11-20 12:27:34 +0000

    fixed menu

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import mainTemplate from '../templates/main.html!text'
 import bean from 'fat/bean'
 // import bonzo from 'ded/bonzo'
 import debounce from './lib/debounce'
+import throttle from './lib/throttle'
 import {fetchJSON} from './lib/fetch'
 import share from './lib/share'
 
@@ -30,6 +31,34 @@ function load(el, data) {
         let shareUrl = data.conversations[i].shareUrl || data.shareUrl;
         let hashTag = data.conversations[i].hashtag || data.hashtag;
         share(shareText, shareUrl, hashTag)(network);
+    })
+
+    let els = {
+        top: el.querySelector('#cnv-top'),
+        menu: el.querySelector('.cnv-fixed-menu'),
+        menuTitle: el.querySelector('.cnv-fixed-menu__title'),
+        convos: [].slice.call(el.querySelectorAll('.cnv-conversation')).reverse()
+    }
+    let menuVisible = false;
+    let showMenu = () => els.menu.setAttribute('data-show', '');
+    let hideMenu = () => els.menu.removeAttribute('data-show');
+
+    function setMenuTitle() {
+        for (var i = 0; i < els.convos.length; i++) {
+            let {top} = els.convos[i].getBoundingClientRect();
+            console.log(top, top < 0);
+            if (top < 0) return els.menuTitle.textContent = els.convos[i].getAttribute('data-title');
+        }
+    }
+    let throttledSetMenuTitle = throttle(setMenuTitle, 150, {leading: false, trailing: true});
+
+    window.addEventListener('scroll', evt => {
+        let menuShouldBeVisible = els.top.getBoundingClientRect().bottom < 0;
+        if (menuShouldBeVisible) {
+            throttledSetMenuTitle()
+            if (!menuVisible) showMenu();
+        } else if (menuVisible) hideMenu();
+        menuVisible = menuShouldBeVisible;
     })
 }
 
